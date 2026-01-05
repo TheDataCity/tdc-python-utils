84471f434261ba009d8afa6b6382da58977e35f9
Luke Strange <92686634+luke-strange@users.noreply.github.com>
2026-01-05 16:40:20 +0000

    Update azure_storage_downloader.py
    Add timedelta support to DateHandler.now()

--- src/tdc_python_utils/date_handler.py
+++ src/tdc_python_utils/date_handler.py
@@ -1,22 +1,28 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 
 class DateHandler:
     def __init__(self):
         self.first_day_this_month = self.now(format="%Y-%m") + '-01'
         self.today_isoformat = self.now(format="%Y-%m-%d")
+        self.first_of_prev_month = datetime.now().replace(day=1) - timedelta(days=1)
+        self.prev_month_year = self.first_of_prev_month.strftime("%b%Y")
         pass
     
-    def now(self, format:str = "%b%Y") -> str:
+    def now(format:str = "%b%Y", **delta:timedelta) -> str:
         """
-        Helper function to return the current time in the specified format
+        Helper function to return the current time (or timedelta from now) in the specified format.
         
         Params
         ------
         format: str (default = "%b%Y")
             Any suitable stftime format string, see: https://strftime.org/
 
+        delta: datetime.timedelta 
+            days, seconds, microseconds, milliseconds, hours, minutes, weeks
+
         Returns
         -------
         The current datetime object in the specified format as a string
         """
-        return (datetime.now()).strftime(format)
+        td = timedelta(**delta) if delta else timedelta(days=0)
+        return (datetime.now() - td).strftime(format)
